8e8be73a7abcfaa509fe1ccf9b2158fe0d07c07d
RobinHankin <hankin.robin@gmail.com>
2019-07-26 19:49:01 +1200

    whitespace

--- R/aaa.R
+++ R/aaa.R
@@ -1,15 +1,16 @@
-`modulo` <- function(n){   # workflow modelled on sol() in the lorentz() package
-  if(missing(n)){  # return modulus
-    return(getOption("M"))
-  } else {  # working modulus supplied
-
-    stopifnot(is.numeric(n))
-    stopifnot(length(n)==1)
-    stopifnot(n==round(n))
-    stopifnot(n>1)
-
-    options("M" = n)
-    options("prompt" = paste("Modulo(", n, ") > ",sep=""))
-  }
+`modulus` <- function(n){   # workflow modelled on sol() in the lorentz() package
+    if(missing(n)){  # return modulus
+        return(getOption("M"))
+    } else {  # working modulus supplied
+        
+        stopifnot(is.numeric(n))
+        stopifnot(length(n)==1)
+        stopifnot(n==round(n))
+        stopifnot(n>1)
+        
+        options("M" = n)
+        options("phi" = numbers::eulersPhi(n))
+        options("prompt" = paste("Modulo(", n, ") > ",sep=""))
+    }
 }
 

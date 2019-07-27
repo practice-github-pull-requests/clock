7c587e09fda0cab9131ef27a2b875b840dd1b5c0
RobinHankin <hankin.robin@gmail.com>
2019-07-28 11:57:57 +1200

    change the class name from "mod" to "clock"

--- R/modulo.R
+++ R/modulo.R
@@ -1,6 +1,7 @@
-`mod` <- setClass("mod", contains = "integer")
+`clock` <- setClass("clock", contains = "integer")
+setAs(from="clock"  ,to="numeric", function(from){  return(from@.Data)})  # No occurences of '@' below here.
 
-setValidity("mod", 
+setValidity("clock", 
             function(object){
               x <- as.numeric(object)
               x <- x[!is.na(x)]  # NA or NaN entries are fine
@@ -18,71 +19,71 @@ setValidity("mod",
             }
             )
 
-setMethod("initialize", "mod", 
+setMethod("initialize", "clock", 
           function(.Object, ...) {
               .Object <- callNextMethod()
               .Object
           }
           )
 
-setAs(from="mod"    ,to="numeric", function(from){  return(from@.Data)})  # No occurences of '@' below here.
-setAs(from="numeric",to="mod",def=function(from){mod(as.integer(round(from)) %% modulus())})
-setAs(from="integer",to="mod",def=function(from){mod(from %% modulus())})
+setAs(from="numeric",to="clock",def=function(from){clock(as.integer(round(from)) %% modulus())})
+setAs(from="integer",to="clock",def=function(from){clock(from %% modulus())})
 
-setMethod("as.numeric",signature(x="mod"),function(x){as(x,"integer")})
+setMethod("as.numeric",signature(x="clock"),function(x){as(x,"integer")})
 
-"is.mod" <- function(x){is(x,"mod")}
-"as.mod" <- function(x){ return(as(x,"mod"))}
+"is.mod" <- function(x){is(x,"clock")}
+"mod"    <- function(x){ return(as(x,"clock"))}
+"as.mod"    <- function(x){ return(as(x,"clock"))}
 
-".mod.print" <- function(x){ as.numeric(x) }
+".clock.print" <- function(x){ as.numeric(x) }
     
-"print.mod" <- function(x, ...){
-  jj <- .mod.print(x, ...)
+"print.clock" <- function(x, ...){
+  jj <- .clock.print(x, ...)
   print(jj)
   cat(paste("members of Z/",modulus(),"Z\n",sep=""))
   return(invisible(jj))
 }
 
-setMethod("show", "mod", function(object){print.mod(object)})
+setMethod("show", "clock", function(object){print.clock(object)})
 
 setGeneric(".cPair", function(x,y){standardGeneric(".cPair")})
-setMethod(".cPair", c("mod", "mod"), function(x,y){.mod.cPair(x,y)})
-setMethod(".cPair", c("mod", "ANY"), function(x,y){.mod.cPair(x,as.mod(y))})
-setMethod(".cPair", c("ANY", "mod"), function(x,y){.mod.cPair(as.mod(x),y)})
+setMethod(".cPair", c("clock", "clock"), function(x,y){.clock.cPair(x,y)})
+setMethod(".cPair", c("clock", "ANY"), function(x,y){.clock.cPair(x,as.mod(y))})
+setMethod(".cPair", c("ANY", "clock"), function(x,y){.clock.cPair(as.mod(x),y)})
 setMethod(".cPair", c("ANY", "ANY"), function(x,y){c(x,y)})
 
-"cmod" <- function(x, ...) {
+"cclock" <- function(x, ...) {
    if(nargs()<3)
       .cPair(x,...)
     else
       .cPair(x, Recall(...))
 }
 
-".mod.cPair" <- function(x,y){
+".clock.cPair" <- function(x,y){
   x <- as.mod(x)
   y <- as.mod(y)
   mod(c(as.numeric(x),as.numeric(y)))
 }
 
-setMethod("sqrt",",mod", function(x){
+setMethod("sqrt","clock", function(x){
   stop("not yet implemented; need Legendre symbols")
 } )
           
-setMethod("Math", "mod",
-          function(x){stop(paste(.Generic, "not allowed on mod objects"))}
+setMethod("Math", "clock",
+          function(x){stop(paste(.Generic, "not allowed on clock objects"))}
           )
 
-".mod.negative" <- function(e1){ as.mod(-as.numeric(e1)) }
-"mod.inverse" <- function(e1){
+".clock.negative" <- function(e1){ as.clock(-as.numeric(e1)) }
+"clock.inverse" <- function(e1){
     stopifnot(is.mod(e1))
     stopifnot(numbers::isPrime(modulus()))
     e1 <- as.numeric(e1)
     e1[e1==0] <- NA
     as.mod(e1^(modulus()-2))
 }
-".mod.add" <- function(e1,e2){ as.mod(as.numeric(e1) + as.numeric(e2)) }
-".mod.mult" <- function(e1,e2){ as.mod(as.numeric(e1)*as.numeric(e2)) }
-".mod.power"<- function(e1,e2){
+".clock.add" <- function(e1,e2){ as.mod(as.numeric(e1) + as.numeric(e2)) }
+".clock.mult" <- function(e1,e2){ as.mod(as.numeric(e1)*as.numeric(e2)) }
+".clock.power"<- function(e1,e2){
     a <- as.numeric(e1)
     if(is.mod(e2)){stop(paste("a^p not defined for p a member of Z/",mod(),sep=""))}
     p <- e2    # notionally calculating a^p
@@ -96,74 +97,74 @@ setMethod("Math", "mod",
     negs <- which(p<0)
     if(any(negs)){
         p <- abs(p)
-        a[negs] <- as.numeric(mod.inverse(as.mod(a[negs])))
+        a[negs] <- as.numeric(clock.inverse(as.mod(a[negs])))
     }
     return(as.mod(a^(p%%phi)))
 }
 
-".mod.quotient" <- function(e1,e2){
+".clock.quotient" <- function(e1,e2){
     stopifnot(is.mod(e2))
-    as.mod(as.numeric(e1)*as.numeric(mod.inverse(e2)))
+    as.mod(as.numeric(e1)*as.numeric(clock.inverse(e2)))
 }
 
-setMethod("Arith",signature(e1 = "mod", e2="missing"),
+setMethod("Arith",signature(e1 = "clock", e2="missing"),
           function(e1,e2){
             switch(.Generic,
                    "+" = e1,
-                   "-" = .mod.negative(e1),
+                   "-" = .clock.negative(e1),
                    stop(paste("Unary operator", .Generic,
-                              "not allowed on mod objects"))
+                              "not allowed on clock objects"))
                    )
           } )
 
-".mod.arith" <- function(e1,e2){
+".clock.arith" <- function(e1,e2){
   switch(.Generic,
-         "+" = .mod.add  (e1, e2),
-         "-" = .mod.add  (e1, .mod.negative(as.mod(e2))),
-         "*" = .mod.mult (e1, e2),
-         "/" = .mod.quotient(e1,e2),
-         "^" = .mod.power(e1, e2),
-         stop(paste("binary operator \"", .Generic, "\" not defined for mod objects"))
+         "+" = .clock.add  (e1, e2),
+         "-" = .clock.add  (e1, .clock.negative(as.mod(e2))),
+         "*" = .clock.mult (e1, e2),
+         "/" = .clock.quotient(e1,e2),
+         "^" = .clock.power(e1, e2),
+         stop(paste("binary operator \"", .Generic, "\" not defined for clock objects"))
          ) }
 
-setMethod("Arith", signature(e1 = "mod", e2="ANY"), .mod.arith)
-setMethod("Arith", signature(e1 = "ANY", e2="mod"), .mod.arith)
-setMethod("Arith", signature(e1 = "mod", e2="mod"), .mod.arith)
+setMethod("Arith", signature(e1 = "clock", e2="ANY"), .clock.arith)
+setMethod("Arith", signature(e1 = "ANY", e2="clock"), .clock.arith)
+setMethod("Arith", signature(e1 = "clock", e2="clock"), .clock.arith)
 
 
-".mod.equal" <- function(e1,e2){ as.numeric(e1) == as.numeric(e2) }
+".clock.equal" <- function(e1,e2){ as.numeric(e1) == as.numeric(e2) }
 
-".mod.greater" <- function(e1,e2){
+".clock.greater" <- function(e1,e2){
   stop(paste("Z/",modulus(),"Z is not an ordered set.\n",sep=""))
 }
 
-".mod.compare" <- function(e1,e2){
+".clock.compare" <- function(e1,e2){
 
   e1 <- as.mod(e1)
   e2 <- as.mod(e2)
   switch(.Generic,
-         "==" =  .mod.equal(e1,e2),
-         "!=" = !.mod.equal(e1,e2),
-         ">"  =  .mod.greater(e1,e2),
-         "<"  = !.mod.greater(e1,e2) & !.mod.equal(e1,e2),
-         ">=" =  .mod.greater(e1,e2) |  .mod.equal(e1,e2),
-         "<=" = !.mod.greater(e1,e2) |  .mod.equal(e1,e2),
-         stop(paste(.Generic, "not supported for mod objects"))
+         "==" =  .clock.equal(e1,e2),
+         "!=" = !.clock.equal(e1,e2),
+         ">"  =  .clock.greater(e1,e2),
+         "<"  = !.clock.greater(e1,e2) & !.clock.equal(e1,e2),
+         ">=" =  .clock.greater(e1,e2) |  .clock.equal(e1,e2),
+         "<=" = !.clock.greater(e1,e2) |  .clock.equal(e1,e2),
+         stop(paste(.Generic, "not supported for clock objects"))
          )
 }
 
-setMethod("Compare", signature(e1="mod", e2="ANY"), .mod.compare)
-setMethod("Compare", signature(e1="ANY", e2="mod"), .mod.compare)
-setMethod("Compare", signature(e1="mod", e2="mod"), .mod.compare)
+setMethod("Compare", signature(e1="clock", e2="ANY"), .clock.compare)
+setMethod("Compare", signature(e1="ANY", e2="clock"), .clock.compare)
+setMethod("Compare", signature(e1="clock", e2="clock"), .clock.compare)
 
-".mod.logic" <- function(e1,e2){
-  stop("No logic currently implemented for mod objects")
+".clock.logic" <- function(e1,e2){
+  stop("No logic currently implemented for clock objects")
 }
 
 ## The following lines deal with idiom like mod(1:10) & mod(10:1) [which are meaningless]
-setMethod("Logic",signature(e1="mod",e2="ANY"), .mod.logic)
-setMethod("Logic",signature(e1="ANY",e2="mod"), .mod.logic)
-setMethod("Logic",signature(e1="mod",e2="mod"), .mod.logic)
+setMethod("Logic",signature(e1="clock",e2="ANY"), .clock.logic)
+setMethod("Logic",signature(e1="ANY",e2="clock"), .clock.logic)
+setMethod("Logic",signature(e1="clock",e2="clock"), .clock.logic)
 
 if(!isGeneric("prod")){   # prod(1:10) returns factorial(10); cf Wilson's theorem
 setGeneric("prod", function(x, ..., na.rm = FALSE)
@@ -189,18 +190,18 @@ setGeneric("sum", function(x, ..., na.rm = FALSE)
 	group = "Summary")
 }
 
-".mod.prod" <- function(x){  
+".clock.prod" <- function(x){  
     stop("not yet implemented")
 }
 
-".mod.sum" <- function(x){ mod(as.numeric(x)) }
+".clock.sum" <- function(x){ mod(as.numeric(x)) }
 
-setMethod("Summary", "mod",
+setMethod("Summary", "clock",
           function(x, ..., na.rm=FALSE){
             switch(.Generic,
-                   prod   =  .mod.prod(x),
-                   sum    =  .mod.sum(x),
-                   stop(paste(.Generic, "not implemented on mod objects"))
+                   prod   =  .clock.prod(x),
+                   sum    =  .clock.sum(x),
+                   stop(paste(.Generic, "not implemented on clock objects"))
                    )
           }
           )

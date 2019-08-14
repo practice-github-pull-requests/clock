89595615971ada37433e6f648f0317df7a6825f5
practice-github-pull-requests <54090922+practice-github-pull-requests@users.noreply.github.com>
2019-08-14 17:03:05 +1200

    Update zzz.R
    more docs

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-`.onLoad` <- function(libname, pkgname) {
+`.onLoad` <- function(libname, pkgname) { # set the prompt
     options(M=NA)
     options("prompt" = "The package will return gibberish until you set the working modulus with modulus().  Type something like 'modulus(7)' to get started.  ")
 }
